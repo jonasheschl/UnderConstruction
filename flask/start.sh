fc323a2a424f72b616a843a532a8893d34975728
Jonas Heschl <heschl@protonmail.com>
2024-12-11 00:33:46 +0100

    Fix scripts relying on +x permissions

--- flask/start.sh
+++ flask/start.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/tmp/cleanup.sh &
+sh /tmp/cleanup.sh &
 while true; do
   gunicorn -b 0.0.0.0:1337 -k gevent "app:create_app()"
 done
